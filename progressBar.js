cde04314794f3c223b497aa470d819b6386544af
Victor Powilleit <victor.powilleit@gmail.com>
2023-12-26 17:59:07 -0300

    Create progressBar.js
    A progress bar for console/terminal wich receives 4 paramenter, explained bellow.
    
    bruteProgress:
     Simply the actual iteration of the measured process divided by the maximum iterations it shall run.
    
    length (optional):
     The length (in characters) the bar will take when completed.
     The default value is 20
    
    stepsToPrint (optional):
     Defines how many prints the bar will have until it reaches 100%
     It's default value is set to 100, so one print for every 1% of the bar. Larger numbers make the bar progress smoother, while smaller values may feel "bumpy".
    
    index (optional):
     This last value was included for cases where more than a single bar is placed at the same time (for handling sub-processes, for example, or deal with async processes). It simply defines a number (an index) to save the values used by each bar, so they won`t mess up.
    
    The usage is simple:
     Call showProgressBar as the first line inside your iteration cycle and then, as it can be really tricky to get a loop to properly send a "1" when it's life-cycle is over, is recommended to call it once again right after the end of it's iterations passing (1) as the bruteProgress param, granting then it will surely get to end.

---
+++ progressBar.js
@@ -0,0 +1,45 @@
+export function showProgressBar(bruteProgress, length = 20, stepsToPrint = 100, index = 0){
+    if(typeof globalThis['SPB_'+index] === 'undefined'){
+        globalThis['SPB_'+index] = {
+            lastRegisteredProgress: 0,
+            bar: Array(length).fill(' '),
+            steps: stepsToPrint/100,
+            blockProgressValue: 100/length,
+            blockProgressStep: (100/length)/9
+        }
+    }
+    const SPB_ = globalThis['SPB_'+index]
+    if(bruteProgress===1){
+        const spacer = '   '
+        SPB_.bar.fill('\u2588')
+        process.stdout.write(SPB_.bar.join('')+' 100%'+spacer+'\r\n')
+        delete globalThis['SPB_'+index]
+        return
+    }
+    const progress = bruteProgress*100
+    const stepProgress = Math.floor(progress*SPB_.steps)
+
+    if(SPB_.lastRegisteredProgress === stepProgress){return}
+    SPB_.lastRegisteredProgress = stepProgress
+
+    const formattedProgress = progress.toFixed(1)
+    const completedBlocks = Math.floor(progress/SPB_.blockProgressValue)
+    const progressLeft = progress%SPB_.blockProgressValue
+    const graphic = Math.floor(progressLeft/SPB_.blockProgressStep)
+    let block = ''
+    switch (graphic) {
+        case 1: block = '\u258f'; break
+        case 2: block = '\u258e'; break
+        case 3: block = '\u258d'; break
+        case 4: block = '\u258c'; break
+        case 5: block = '\u258b'; break
+        case 6: block = '\u258a'; break
+        case 7: block = '\u2589'; break
+        case 8: block = '\u2588'; break
+        default: block = ''; break
+    }
+    SPB_.bar.fill(' ')
+    SPB_.bar.fill('\u2588', 0, completedBlocks)
+    SPB_.bar[completedBlocks] = block
+    process.stdout.write(SPB_.bar.join('')+' '+formattedProgress+'% '+'\r')
+}
